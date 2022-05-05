e95e194d9faaa9315946a8934f8fcf9a2feef006
gbrlbrbs <gabrielbarbosamartinz_@outlook.com>
2022-05-05 20:53:45 -0300

    pseudocode

--- lab2.m
+++ lab2.m
@@ -22,4 +22,33 @@ for i=1:length(ns)
 end
 
 
-% split 
+%item 3:
+% função que recebe como parâmetros:u(D), que é a informação,
+% e g(D)'s gerados no item 2 (tantos quantos forem os pares (n,k))
+
+%item 4:
+% após v(D) passar pelo canal, sai do canal r(D) = v(D) + e(D)
+
+% o resto de r(D)/g(D) é o mesmo resto de e(D)/g(D) e se chama síndrome
+% (s(D))
+
+% compara-se essa síndrome com o conjunto de síndromes estabelecidas como
+% as síndromes de quando há erro na primeira posição de r(D)
+
+% se a comparação der verdadeira, significa que há erro na primeira posição
+% de r(D). Troca-se o primeiro bit de r(D) (para corrigir o erro) e tem-se
+% o vetor desejado
+
+% se a comparação der falsa, significa que não há erro na primeira posição
+%de r(D). Nesse caso, rotaciona-se r(D) e s(D), obtendo-se r_1(D) e
+%s_1(D). Novamente compara-se s_1(D) com o conjunto de síndromes de quando
+%há erro na primeira posição. Caso encontre erro, corrige-se o erro e esse é o
+%vetor desejado. Caso não encontre erro, o processo de rotação continua até
+%o erro ser encontrado
+
+% é preciso armazenar o número de rotações feitas em uma variável para
+% poder "desrotacionar" o vetor e recolocar as posições dele (corrigidas)
+% nos devidos lugares
+
+%precisa-se de funções para rotacionar r(D) em relação a (1+D^N) e s(D)
+%em relação a g(D) 
